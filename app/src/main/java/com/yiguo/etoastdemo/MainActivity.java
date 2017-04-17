0598f05ed4bbe883d3da16fb5dff3d96eeaa61eb
zhaocheng <zhaocheng>
2017-04-17 16:15:08 +0800

    取出不必要的依赖包

--- app/src/main/java/com/yiguo/etoastdemo/MainActivity.java
+++ app/src/main/java/com/yiguo/etoastdemo/MainActivity.java
@@ -1,12 +1,12 @@
 package com.yiguo.etoastdemo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.yiguo.toast.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
